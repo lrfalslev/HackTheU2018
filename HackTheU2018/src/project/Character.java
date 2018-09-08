906d6af3dd53cf7713f38a121e088301208ce9ce
lrfalslev <37004519+lrfalslev@users.noreply.github.com>
2018-09-08 15:53:07 -0600

    make new classes

---
+++ HackTheU2018/src/project/Character.java
@@ -0,0 +1,11 @@
+package project;
+
+public class Character {
+	
+	
+
+	}
+	
+	
+
+
--- HackTheU2018/src/project/Project.java
+++
@@ -1,9 +0,0 @@
-package project;
-
-public class Project {
-
-	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
-
-}
