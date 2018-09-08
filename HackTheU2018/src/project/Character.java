d952eaeab2d07c451c7c4f83242a54b920c25679
Oliver <Oliver@DESKTOP-GNK3ARA.uconnect.utah.edu>
2018-09-08 17:47:06 -0600

    class features

--- HackTheU2018/src/project/Character.java
+++ HackTheU2018/src/project/Character.java
@@ -13,6 +13,15 @@ public class Character {
 		String[] bardFeats = new String[] {"Spellcasting", "Bardic Inspiration"};
 		String[] clericFeats = new String[] {"Spellcasting", "Divine Domain"};
 		String[] druidFeats = new String[] {"Druidic", "Spellcasting"};
+		String[] fightFeats = new String[] {"Fighting Style", "Second Wind"};
+		String[] monkFeats = new String[] {"Unarmored Defense", "Martial Arts"};
+		String[] pallyFeats = new String[] {"Divine Sense", "Lay on Hands"};
+		String[] rangFeats = new String[] {"Favored Enemy", "Natural Explorer"};
+		String[] rogFeats = new String[] {"Expertise", "Sneak Attack", "Thieves' Cant"};
+		String[] sorcFeats = new String[] {"Spellcasting", "Sorcerous Origin"};
+		String[] warFeats = new String[] {"Otherworldly Patron", "Pact Magic"};
+		String[] wizFeats = new String[] {"Spellcasting", "Arcane Recovery"};
+		
 		
 		
 	}
