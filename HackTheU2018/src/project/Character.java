e9f0ca9d78ed2a46177679fed3f07528722f3ed6
Oliver <Oliver@DESKTOP-GNK3ARA.uconnect.utah.edu>
2018-09-08 17:33:42 -0600

    array character/race info

--- HackTheU2018/src/project/Character.java
+++ HackTheU2018/src/project/Character.java
@@ -1,11 +1,20 @@
 package project;
 
 public class Character {
-	
-	
 
+	public static void main(String args[]) {
+		String[] races = new String[] {  "Dragonborn", "Dwarf", "Elf", "Gnome", "Half-Elf",
+				"Halfling", "Half-Orc", "Human","Lizardfolk", "Orc", "Tabaxi", "Tiefling" };
+
+		String[] classes = new String[] { "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk",
+				"Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard" };
+		
+		String[] barbFeats = new String[] {"Rage", "Unarmored Defense"};
+		String[] bardFeats = new String[] {"Spellcasting", "Bardic Inspiration"};
+		String[] clericFeats = new String[] {"Spellcasting", "Divine Domain"};
+		String[] druidFeats = new String[] {"Druidic", "Spellcasting"};
+		
+		
 	}
-	
-	
 
-
+}
