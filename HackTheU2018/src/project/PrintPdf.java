8422c72f5ffff6abca39249fff7446be1478b467
lrfalslev <37004519+lrfalslev@users.noreply.github.com>
2018-09-08 23:50:19 -0600

    add pdfbox

--- HackTheU2018/src/project/PrintPdf.java
+++ HackTheU2018/src/project/PrintPdf.java
@@ -1,5 +1,18 @@
 package project;
 
+import java.io.File;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+
 public class PrintPdf {
+	
+	try {
+		
+	}
+	
+	File sheet = new File("/HackTheU2018/Character Sheet - Form Fillable.pdf");
+	
+	PDDocument pd = PDDocument.load(sheet);
+	
 	
 }
