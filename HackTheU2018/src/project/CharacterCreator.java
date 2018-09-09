d04304210925fed8fb2d06d316eaf2b3344083f8
lrfalslev <37004519+lrfalslev@users.noreply.github.com>
2018-09-08 20:35:48 -0600

    builds character generator

--- HackTheU2018/src/project/CharacterCreator.java
+++ HackTheU2018/src/project/CharacterCreator.java
@@ -1,13 +1,34 @@
 package project;
 
+import java.util.ArrayList;
+
 public class CharacterCreator {
 
-	Character myCharacter = new Character();
+	static Character myCharacter;
 
 	public static void main(String[] args) {
-
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() { project.GUI.createAndShowGUI();}
-		});
+		
+		String name;
+		String charClass;
+		String race;
+		String Alignment;
+		int[] traits;
+		int[] buffs = new int[6];
+		ArrayList<String> feats = new ArrayList<String>();
+		
+		Character.getRace(feats, buffs);
+		
+		traits = roller.Roll();
+		
+		for(int idx = 0; idx < 6; idx++)
+		{
+			traits[idx] += 1;
+		}
+		
+		
+		//gui window
+//		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//			public void run() { project.GUI.createAndShowGUI();}
+//		});
 	}
 }
