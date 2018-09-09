84b1f5a846441740cb4b0890634d7571f2bb8b3a
Kingpretzl <37088615+Kingpretzl@users.noreply.github.com>
2018-09-08 18:41:06 -0600

    Update Character.java

--- HackTheU2018/src/project/Character.java
+++ HackTheU2018/src/project/Character.java
@@ -8,6 +8,7 @@ public class Character {
 		String[] classes = new String[] { "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk",
 				"Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard" };
 		
+		// Feats
 		String[] barbFeats = new String[] {"Rage", "Unarmored Defense"};
 		String[] bardFeats = new String[] {"Spellcasting", "Bardic Inspiration"};
 		String[] clericFeats = new String[] {"Spellcasting", "Divine Domain"};
@@ -21,9 +22,19 @@ public class Character {
 		String[] warFeats = new String[] {"Otherworldly Patron", "Pact Magic"};
 		String[] wizFeats = new String[] {"Spellcasting", "Arcane Recovery"};
 		
-		int blah;
-		
-		int asgfijfasd;
+		// Buffs
+		int[] dragBuff = {2,0,0,0,0,1};
+		int[] dwarfBuff = {0,0,2,0,0,0};
+		int[] elfBuff = {0,2,0,0,0,0};
+		int[] gnomeBuff = {0,0,0,2,0,0};
+		int[] halfElfBuff = {0,0,0,1,1,2};
+		int[] halfBuff = {0,2,0,0,0,0};
+		int[] halfOrcBuff = {2,0,1,0,0,0};
+		int[] humanBuff = {1,1,1,1,1,1};
+		int[] lizBuff = {0,0,2,0,1,0};
+		int[] orcBuff = {2,0,1,-2,0,0};
+		int[] baxiBuff = {0,2,0,0,0,1};
+		int[] tieBuff = {0,0,0,1,0,2};
 	}
 }
 
