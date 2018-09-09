5620c35c586bd008e556cd6ecb64b6db01dda6cb
Kingpretzel <mildonjakob@gmail.com>
2018-09-08 22:41:30 -0600

    Keep

--- HackTheU2018/src/project/spells.java
+++ HackTheU2018/src/project/spells.java
@@ -2,19 +2,28 @@ package project;
 
 public class spells  {
         public static String[] getSpells(String CharacterClass){
+
+            spellList = "";
             if (CharacterClass = "Wizard") {
-                return String[]"Mage Hand","Fire Bolt", "Prestidigitation", "Burning Hands", "Disguise Self", "Detect Magic",
+                spellList = "Mage Hand","Fire Bolt", "Prestidigitation", "Burning Hands", "Disguise Self", "Detect Magic",
                 "Magic Missile", "Mage Armor", "Thunderwave";
             } else if (CharacterClass = "Bard") {
-                return String[]
-                "Vicious Mockery", 'Friends', "Cure Wounds", "Silent Image", "Thunderwave", "Tasha's Hideous Laughter";
+                spellList = "Vicious Mockery", "Friends", "Cure Wounds", "Silent Image", "Thunderwave",
+                        "Tasha's Hideous Laughter";
             } else if (CharacterClass = "Cleric") {
-                return String[]"Spare the Dying", "Guidance", "Word of Radiance";
+                spellList = "Spare the Dying", "Guidance", "Word of Radiance";
+            } else if (CharacterClass = "Druid") {
+                spellList = "Primal Savagery", "Thorn Whip";
+            }else if (CharacterClass = "Sorcerer") {
+                spellList = "Thunderclap", "Mage Hand", "Prestidigitation", "Ray of Frost", "Witch Bolt", "Fog Cloud";
             }
+            else if (CharacterClass = "Warlock") {
+                spellList = "Eldritch Blast", "Green-Flame Blade";
+            }
+            else;
+            spellList = "None";
             }
 
 
 
         }
-
-}
