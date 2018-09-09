8064fc553759de1a6710fba24520a446764ed353
Kingpretzel <mildonjakob@gmail.com>
2018-09-09 07:41:43 -0600

    Dong ding

--- HackTheU2018/src/project/hitPoints.java
+++ HackTheU2018/src/project/hitPoints.java
@@ -1,39 +1,40 @@
 package project;
-// Calculates the HP of character based on their chosen class.
+// Calculates the HP of character based on their chosen class and Con Mod.
 public class hitPoints {
     public static int getHit;
     String[] classes;
     String[] classtype;
     int conMod;
+    int hpMax;
 
-    {int hp = 0;
+    {
 
         if (classtype.equals("Barbarian")) {
-            int hp = 12 + conMod;
+            int hpMax = 12 + conMod;
         } else if (classtype.equals("Bard")) {
-            int hp = 8 + conMod;
+            int hpMax = 8 + conMod;
         } else if (classtype.equals("Cleric")) {
-            int hp = 8 + conMod;
+            int hpMax = 8 + conMod;
         } else if (classtype.equals("Druid")) {
-            int hp = 8 + conMod;
+            int hpMax = 8 + conMod;
         } else if (classtype.equals("Fighter")) {
-            int hp = 8 + conMod;
+            int hpMax = 8 + conMod;
         } else if (classtype.equals("Monk")) {
-            int hp = 8 + conMod;
+            int hpMax = 8 + conMod;
         } else if (classtype.equals("Paladin")) {
-            int hp = 10 + conMod;
+            int hpMax = 10 + conMod;
         } else if (classtype.equals("Ranger")) {
-            int hp = 10 + conMod;
+            int hpMax = 10 + conMod;
         } else if (classtype.equals("Rogue")) {
-            int hp = 8 + conMod;
+            int hpMax = 8 + conMod;
         } else if (classtype.equals("Sorcerer")) {
-            int hp = 6 + conMod;
+            int hpMax = 6 + conMod;
         } else if (classtype.equals("Warlock")) {
-            int hp = 8 + conMod;
+            int hpMax = 8 + conMod;
         } else {
-            int hp = 6 + conMod;
+            int hpMax = 6 + conMod;
         }
-        return hp;
+
 
     }}
     // (Objects.equals(classtype, "Barbarian"))
